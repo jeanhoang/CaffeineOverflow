12d0800e821ac3ea5825384d97cb9ee297b62272
Mark Zarak <zarak@sheridancollege.ca>
2022-03-13 10:38:45 -0400

    Created cart as a modal

--- sevla-design-studio/src/App.js
+++ sevla-design-studio/src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from 'react';
+
 import Header from "./components/Layout/Header";
 import Products from './components/Products/Products';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Products />
---
+++ sevla-design-studio/src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import Modal from '../UI/Modal';
+
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: '1', name: 'Keychain', amount: 2, price: 22.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>45.98</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  )
+};
+
+export default Cart;
