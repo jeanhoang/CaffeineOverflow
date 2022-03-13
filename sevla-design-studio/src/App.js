3e82a871327a3051a05b098491f2d0b7dc2aa451
Mark Zarak <zarak@sheridancollege.ca>
2022-03-13 14:56:55 -0400

    Implimented hook to globally store cart data

--- sevla-design-studio/src/App.js
+++ sevla-design-studio/src/App.js
@@ -1,8 +1,9 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 
 import Header from "./components/Layout/Header";
 import Products from './components/Products/Products';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
 
@@ -17,13 +18,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Products />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- sevla-design-studio/src/components/Layout/HeaderCartButton.js
+++ sevla-design-studio/src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,25 @@
+import { useContext } from 'react';
+
+import CartContext from '../../store/cart-context';
+
 import classes from './HeaderCartButton.module.css';
 import cartIcon from '../../assets/cart.png';
 
 const HeaderCartButton = props => {
+  const cartContext = useContext(CartContext);
+
+  let numberOfCartItems = 0;
+
+  for (let i = 0; i < cartContext.items.length; i++) {
+    numberOfCartItems += cartContext.items[i].amount;
+  }
+
   return <button className={classes.button} onClick={props.onClick} >
     <span className={classes.icon}>
       <img src={cartIcon} alt='Cart' />
     </span>
     <span>Your Cart</span>
-    <span className={classes.badge}>3</span>
+    <span className={classes.badge}>{numberOfCartItems}</span>
   </button>
 };
 
