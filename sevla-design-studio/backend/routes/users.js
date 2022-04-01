223c165c9c3ac66c708bc7826efe0f4d97f2f6a4
Christopher Lewis <c_s_lewis@hotmail.com>
2022-04-01 12:20:33 -0400

    encrypt commit

--- sevla-design-studio/backend/routes/users.js
+++ sevla-design-studio/backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 let User = require('../models/user.models');
+const bcrypt = require('bcrypt');
 
 router.route('/').get((req, res) => {
     User.find()
@@ -7,15 +8,18 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const newUser = new User({username, password});
-
-    newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route('/add').post(async(req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const username = req.body.username;
+        const newUser = new User({username, hashedPassword});
+        newUser.save()
+        .then(() => res.json('User added!'))
+    }
+    catch {
+        res.status(400).json('Error: ' + err);
+    }
 });
 
 router.route('/:username').get((req, res) => {
