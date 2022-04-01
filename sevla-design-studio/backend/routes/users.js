189836a9f9b29b70c4f1297dd8841f14820d53cd
Christopher Lewis <c_s_lewis@hotmail.com>
2022-04-01 15:32:11 -0400

    Added encrypted registration and login. Double check .env database before merging/deployment

--- sevla-design-studio/backend/routes/users.js
+++ sevla-design-studio/backend/routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 let User = require('../models/user.models');
 const bcrypt = require('bcrypt');
 
+//for development purposes only
 router.route('/').get((req, res) => {
     User.find()
     .then(users => res.json(users))
@@ -11,15 +12,33 @@ router.route('/').get((req, res) => {
 router.route('/add').post(async(req, res) => {
     try {
         const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const password = await bcrypt.hash(req.body.password, salt)
         const username = req.body.username;
-        const newUser = new User({username, hashedPassword});
+        const newUser = new User({username, password});
         newUser.save()
         .then(() => res.json('User added!'))
     }
     catch {
         res.status(400).json('Error: ' + err);
     }
+});
+
+router.route('/login').post(async(req, res) => {
+    const user = await User.findOne({username: req.body.username})
+    if (user == null) {
+        return res.status(400).json('User not found');
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.json('Credentials matched')
+        } else {
+            res.json('Wrong password')
+        }
+    }
+    catch {
+        res.status(400).json('Error');
+    }
+    
 });
 
 router.route('/:username').get((req, res) => {
