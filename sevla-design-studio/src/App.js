90b80e5149c0f51dd2ceccc810ff9787fb3b7409
Jean Hoang <hoantran@shernet.sheridancollege.ca>
2022-04-01 13:23:26 -0400

    Refactor navigation

--- sevla-design-studio/src/App.js
+++ sevla-design-studio/src/App.js
@@ -1,9 +1,33 @@
-import NavBar from './components/NavigationBar/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AboutPage from './Pages/AboutPage';
+import AuthPage from './Pages/AuthPage';
+import HomePage from './Pages/HomePage';
+import Shopping from './Pages/Shopping';
 
 function App() {
   return (
-      <NavBar/>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/about' exact>
+          <AboutPage />
+        </Route>
+        <Route path='/shopping' exact>
+          <Shopping />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
---
+++ sevla-design-studio/src/components/Layout/MainNavigation.js
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>Sevla Studio</div>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/shopping'>Shopping</Link>
+          </li>
+          <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
