5cab7250adb261d98d1c71f648a9ef77a2f8b943
mohsenas <amohseni80@yahoo.com>
2022-04-04 11:19:44 -0400

    Changed server.js file for deployment purposes

--- sevla-design-studio/backend/server.js
+++ sevla-design-studio/backend/server.js
@@ -5,12 +5,24 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+app.set('port', process.env.PORT || 5000);
+console.log("++++++++++++++++" + app.get('port'));
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('./client/build'));
+
+
+
 const uri = process.env.ATLAS_URI;
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build",     
+    "index.html"));
+ });
+ 
 mongoose.connect(uri);
 
 const connection = mongoose.connection;
