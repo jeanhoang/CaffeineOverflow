235c3a4e16165bfe5ec00da90f516d9e470eae16
Jean Hoang <hoantran@shernet.sheridancollege.ca>
2022-04-03 22:08:54 -0400

    Add about page

--- sevla-design-studio/src/components/StartingPage/StartingPageContent.js
+++ sevla-design-studio/src/components/StartingPage/StartingPageContent.js
@@ -3,7 +3,9 @@ import classes from './StartingPageContent.module.css';
 const StartingPageContent = () => {
   return (
     <section className={classes.starting}>
+      <br></br>
       <h1>Welcome to Sevla Design Studio</h1>
+      <h2>Shop Coming Soon!</h2>
     </section>
   );
 };
