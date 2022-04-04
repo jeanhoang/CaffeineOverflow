928674a4ddeedfc573053e3908042174573de9dd
mohsenas <amohseni80@yahoo.com>
2022-04-04 14:06:56 -0400

    fixed the server error

--- sevla-design-studio/backend/dist/server.dev.js
+++ sevla-design-studio/backend/dist/server.dev.js
@@ -8,16 +8,16 @@ var mongoose = require('mongoose');
 
 require('dotenv').config();
 
-var app = express(); // const port = process.env.PORT || 5000;
+var app = express();
+var port = process.env.PORT || 5000; // app.set('port', process.env.PORT || 5000);
+// console.log("++++++++++++++++" + app.get('port'));
 
-app.set('port', process.env.PORT || 5000);
-console.log("++++++++++++++++" + app.get('port'));
 app.use(cors());
-app.use(express.json());
-app.use(express["static"]('./client/build'));
+app.use(express.json()); // app.use(express.static('../build'));
+
 var uri = process.env.ATLAS_URI;
 app.get("*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "sevla-design-studio", "build", "index.html"));
 });
 mongoose.connect(uri);
 var connection = mongoose.connection;
--- sevla-design-studio/backend/server.js
+++ sevla-design-studio/backend/server.js
@@ -5,21 +5,21 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-// const port = process.env.PORT || 5000;
-app.set('port', process.env.PORT || 5000);
-console.log("++++++++++++++++" + app.get('port'));
+const port = process.env.PORT || 5000;
+// app.set('port', process.env.PORT || 5000);
+// console.log("++++++++++++++++" + app.get('port'));
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('./client/build'));
+// app.use(express.static('../build'));
 
 
 
 const uri = process.env.ATLAS_URI;
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build",     
+    res.sendFile(path.resolve(__dirname, "sevla-design-studio", "build",     
     "index.html"));
  });
  
