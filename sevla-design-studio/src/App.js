356080a4e1222e94c78ec3aa7c1ec8cf74b8cc58
Mark Zarak <zarak@sheridancollege.ca>
2022-04-04 00:44:29 -0400

    Added authentication to the profile page

--- sevla-design-studio/src/App.js
+++ sevla-design-studio/src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, useContext } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
@@ -6,31 +6,41 @@ import AboutPage from './Pages/AboutPage';
 import AuthPage from './Pages/AuthPage';
 import HomePage from './Pages/HomePage';
 import Shopping from './Pages/Shopping';
+import AuthContext from './store/auth-context';
 
 function App() {
+
   return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/about' exact>
-          <AboutPage />
-        </Route>
-        <Route path='/shopping' exact>
-          <Shopping />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-        <Route path='*'>
-          <Redirect to='/' />
-        </Route>
-      </Switch>
-    </Layout>
+    <AuthContext.Consumer>
+      {(ctx) => {
+        return (
+          <Layout>
+            <Switch>
+              <Route path='/' exact>
+                <HomePage />
+              </Route>
+              <Route path='/about' exact>
+                <AboutPage />
+              </Route>
+              <Route path='/shopping' exact>
+                <Shopping />
+              </Route>
+              <Route path='/auth'>
+                <AuthPage />
+              </Route>
+              {ctx.isLoggedIn && (
+                <Route path='/profile'>
+                  <UserProfile />
+                </Route>
+              )}
+              <Route path='*'>
+                <Redirect to='/' />
+              </Route>
+            </Switch>
+          </Layout>
+        )
+      }}
+    </AuthContext.Consumer>
   );
 }
 
