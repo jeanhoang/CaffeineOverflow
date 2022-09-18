bf25800973b629ec37c96659cc70d42376677303
Jean Hoang <44328995+jeanhoang@users.noreply.github.com>
2022-09-18 16:10:06 -0400

    Added connection to fetch data from backend

--- sevla-design-studio/src/components/Products/AvailableProducts.js
+++ sevla-design-studio/src/components/Products/AvailableProducts.js
@@ -2,23 +2,52 @@
 * A component that imports product data and creates a product list.
 */
 
-import ProductData from '../../assets/Data/product-data'
 import Card from '../UI/Card';
 import Item from './Items/Item';
 
 import classes from './AvailableProducts.module.css';
 
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 const AvailableProducts = () => {
+
+  //Initializing product list
+  const [productList, setProductList] = useState([]);
+
+  //API to retrieve product list from backend
+  const getProductListAPI = 'http://localhost:5000/products';
+
+  //Calling getProductList() function
+  useEffect(() => getProductList(), []);
+
+  //A function to fetch data from the backend using axios
+  const getProductList = () => {
+    axios
+      .get(getProductListAPI)
+      .then(response => {
+        console.log(response.data)
+        setProductList(response.data);
+
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
   // A function that wraps each product in an Item component
-  const productList = ProductData.map(product => <Item key={product.id} product={product} />);
+  const displayProductList = productList.map((product) => <Item key={product.id} product={product} />);
+
 
   // Returns a product list wrapped in a card
   return (
-    <section className={classes.products}>
-      <Card>
-        <ul>{productList}</ul>
-      </Card>
-    </section>
+      <section className={classes.products}>
+        <Card>
+          <div>
+            {displayProductList}
+          </div>
+        </Card>
+      </section>
   );
 };
 
