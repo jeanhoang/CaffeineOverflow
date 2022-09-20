9bf10e26fc24be964de5f7f6c937d7d214fd576d
Jean Hoang <44328995+jeanhoang@users.noreply.github.com>
2022-09-20 01:16:17 -0400

    Add link to view detail product upon clicking on Product Name

--- sevla-design-studio/src/components/Products/Items/Item.js
+++ sevla-design-studio/src/components/Products/Items/Item.js
@@ -16,26 +16,27 @@ import { Link } from "react-router-dom";
 const Item = (props) => {
   // A variable that holds the current cart state; the component is re-evaluated whenever it changes
   const cartContext = useContext(CartContext);
-  
-  //Format the price value to 2 decimal places
+
+
   const formattedPrice = `$${props.product.ProductPrice.toFixed(2)}`;
 
   // Adds an item to the cart
   // Called from ItemForm component by user form submission of adding a product to the cart
   const addToCartHandler = amount => {
     cartContext.addItem({
-      id: props.product.id,
+      id: props.product._id,
       name: props.product.ProductName,
       amount: amount,
       price: props.product.ProductPrice
     });
   };
+  
 
   // Returns a single product list item with a name, description, price, number input, and add to cart button
   return (
-    <div className={classes.item}>
+    <div className={classes.item} key={props.product._id}>
       <div className='card'>
-        <h3>{props.product.ProductName}</h3>
+        <h3><Link to={`/products/${props.product.ProductName}`} style={{ textDecoration: 'none' }}>{props.product.ProductName}</Link></h3>
         <div className={classes.description}>{props.product.ProductDescription}</div>
         <div className={classes.price}>{formattedPrice}</div>
         <img src={props.product.ProductImg} alt='' />
