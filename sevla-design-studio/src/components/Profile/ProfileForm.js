bd87fa10e516ea39f797a1e9b717f9b7e55ca791
Mark Zarak <zarak@sheridancollege.ca>
2022-11-07 17:27:31 -0500

    Added css to admin dashboard link

--- sevla-design-studio/src/components/Profile/ProfileForm.js
+++ sevla-design-studio/src/components/Profile/ProfileForm.js
@@ -1,5 +1,6 @@
 import classes from './ProfileForm.module.css';
 import { useRef, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 
 
@@ -9,20 +10,22 @@ const ProfileForm = () => {
   const authCtx = useContext(AuthContext); // Retrieve current user logged in
   const username = authCtx.token;
 
-  const submitHandler = event => {
-    event.preventDefault();
+  const adminLink =
+    <div className={classes.action}>
+      <Link to='/admin/products' className={classes.action}>Admin Dashboard</Link>
+    </div>
 
-    const enteredNewPassword = newPasswordInputRef.current.value;
-
-    // To impliment Post request for updating user password
-
-  };
+  // //To impliment Post request for updating user password
+  // const submitHandler = event => {
+  //   event.preventDefault();
+  //   const enteredNewPassword = newPasswordInputRef.current.value;
+  // };
 
   return (
     <div>
-
       <form className={classes.form}>
         <h1>Welcome, {username}</h1>
+        {username === 'sysadmin' && (adminLink)}
         <div className={classes.control}>
           <label htmlFor='new-password'>New Password</label>
           <input type='password' id='new-password' minLength="8" ref={newPasswordInputRef} />
--- sevla-design-studio/src/components/Profile/UserProfile.js
+++ sevla-design-studio/src/components/Profile/UserProfile.js
@@ -1,19 +1,8 @@
-import { Link } from 'react-router-dom';
 import ProfileForm from './ProfileForm';
-import classes from './UserProfile.module.css';
-import AuthContext from '../../store/auth-context';
-import { useContext } from 'react';
 
 const UserProfile = () => {
-  const authCtx = useContext(AuthContext); // Hook to set or get user account info
-  const username = authCtx.token;
-  const adminLink = <Link to='/admin/products'>Admin Dashboard</Link>
   return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-      {username === 'sysadmin' && (adminLink)}
-    </section>
+    <ProfileForm />
   );
 };
 
