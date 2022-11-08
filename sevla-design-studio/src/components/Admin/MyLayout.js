290c2f10ea928ac59f7ddfd88c59baadb4b9c5e7
Mark Zarak <zarak@sheridancollege.ca>
2022-11-07 20:45:01 -0500

    Removed left menu from admin dashboard

--- sevla-design-studio/src/components/Admin/MyLayout.js
+++ sevla-design-studio/src/components/Admin/MyLayout.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Layout } from 'react-admin';
 import MyAppBar from './MyAppBar';
+import MyMenu from './MyMenu';
 
-const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} />;
+const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} menu={MyMenu} />;
 
 export default MyLayout;
---
+++ sevla-design-studio/src/components/Admin/MyMenu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MyMenu = ({ onMenuClick, logout }) => {
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default MyMenu;
