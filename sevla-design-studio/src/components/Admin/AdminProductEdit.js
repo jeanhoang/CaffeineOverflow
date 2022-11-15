025d5e90149b738c5527f53c77ca795ccbdce74c
Mark Zarak <zarak@sheridancollege.ca>
2022-11-15 00:53:29 -0500

    Improved field validation for admin dashboard

--- sevla-design-studio/src/components/Admin/AdminProductEdit.js
+++ sevla-design-studio/src/components/Admin/AdminProductEdit.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-    Edit,
-    SimpleForm,
-    ReferenceInput,
-    TextInput,
-    Toolbar,
-    SaveButton
-} from 'react-admin';
+import { Edit, SimpleForm, TextInput, Toolbar, SaveButton, minValue, maxValue, maxLength } from 'react-admin';
 
 //Ensure all input fields are required 
 const required = (message = 'Required') =>
@@ -17,30 +10,33 @@ const number = (message = 'Must be a number') =>
     value => value && isNaN(Number(value)) ? message : undefined;
 
 //Ensure inputs must be filled
-const validateTextInput = [required()];
+const validateTextInput = [required(), maxLength(200)];
+
+//Ensure inputs must be filled
+const validateLongTextInput = [required(), maxLength(1000)];
 
 //Ensure price, quantity inputs are required and are numbers
-const validateNum = [required(), number()];
+const validateNum = [required(), number(), minValue(0), maxValue(99999)];
 
 //Create tool bar to edit style for Save Button
 const PostCreateToolbar = props => (
     <Toolbar {...props}>
-        <SaveButton sx={{width: 200}}/>
+        <SaveButton sx={{ width: 750 }} />
     </Toolbar>
-  );
-  
+);
+
 const ProductsEdit = () => {
     return (
-        <Edit sx={{display: 'flex', justifyContent:'center', alignItems:'center', margin: 'auto'}}>
-            <SimpleForm toolbar={<PostCreateToolbar />} sx={{width: 800, alignItems:'center', display: 'flex', padding:'auto'}}>
-                <TextInput source="ProductName" validate={validateTextInput} />
-                <TextInput multiline source="ProductDescription" validate={validateTextInput} sx={{width: 300}}/>
-                <TextInput multiline source="ProductLongDescription" validate={validateTextInput} sx={{width: 500}}/>
-                <TextInput source="ProductPrice" validate={validateNum} />
-                <TextInput source="ProductSize" sx={{width: 150}}/>
-                <TextInput source="ProductType" validate={validateTextInput} sx={{width: 150}}/>
-                <TextInput source="ProductQuantity" validate={validateNum} sx={{width: 150}}/>
-                <TextInput source="ProductImg" validate={validateTextInput} />
+        <Edit sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 'auto' }}>
+            <SimpleForm toolbar={<PostCreateToolbar />} sx={{ width: 800, alignItems: 'center', display: 'flex', padding: 'auto' }}>
+                <TextInput source="ProductName" validate={validateTextInput} sx={{ width: 500 }} />
+                <TextInput source="ProductDescription" validate={validateTextInput} sx={{ width: 500 }} />
+                <TextInput multiline source="ProductLongDescription" validate={validateLongTextInput} sx={{ width: 500 }} />
+                <TextInput source="ProductPrice" validate={validateNum} sx={{ width: 150 }} />
+                <TextInput source="ProductSize" helperText="Optional field." sx={{ width: 150 }} />
+                <TextInput source="ProductType" validate={validateTextInput} sx={{ width: 150 }} />
+                <TextInput source="ProductQuantity" validate={validateNum} sx={{ width: 150 }} />
+                <TextInput multiline source="ProductImg" type="url" helperText="Enter the url to an image." validate={validateLongTextInput} sx={{ width: 770 }} />
             </SimpleForm>
         </Edit>
     )
