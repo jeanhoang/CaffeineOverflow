074e990a92b4f7b4b00291a913b4c87d14f94c44
Jean Hoang <44328995+jeanhoang@users.noreply.github.com>
2022-11-14 22:28:30 -0500

    Added basic CSS for AdminProductCreate and AdminProductEdit

--- sevla-design-studio/src/components/Admin/AdminProductCreate.js
+++ sevla-design-studio/src/components/Admin/AdminProductCreate.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Create, SimpleForm, TextInput } from 'react-admin';
+import { Create, SimpleForm, TextInput, SaveButton, Toolbar } from 'react-admin';
+
 
 //Ensure all input fields are required 
 const required = (message = 'Required') =>
@@ -15,17 +16,24 @@ const validateTextInput = [required()];
 //Ensure price, quantity inputs are required and are numbers
 const validateNum = [required(), number()];
 
+//Create tool bar to edit style for Save Button
+const PostCreateToolbar = props => (
+  <Toolbar {...props}>
+      <SaveButton sx={{width: 200}}/>
+  </Toolbar>
+);
+
 const ProductsCreate = () => {
   return (
-    <Create>
-      <SimpleForm>
+    <Create redirect="show" sx={{display: 'flex', justifyContent:'center', alignItems:'center', margin: 'auto'}}>
+      <SimpleForm toolbar={<PostCreateToolbar />} sx={{width: 800, alignItems:'center', display: 'flex', padding:'auto'}}>
         <TextInput source="ProductName" validate={validateTextInput} />
-        <TextInput multiline source="ProductDescription" validate={validateTextInput} />
-        <TextInput multiline source="ProductLongDescription" validate={validateTextInput} />
-        <TextInput source="ProductPrice" validate={validateNum} />
-        <TextInput source="ProductSize" />
-        <TextInput source="ProductType" validate={validateTextInput} />
-        <TextInput source="ProductQuantity" validate={validateNum} />
+        <TextInput source="ProductDescription" validate={validateTextInput} sx={{width: 300}}/>
+        <TextInput multiline source="ProductLongDescription" validate={validateTextInput} sx={{width: 500}}/>
+        <TextInput source="ProductPrice" validate={validateNum} s/>
+        <TextInput source="ProductSize" sx={{width: 150}}/>
+        <TextInput source="ProductType" validate={validateTextInput}sx={{width: 150}}/>
+        <TextInput source="ProductQuantity" validate={validateNum}sx={{width: 150}}/>
         <TextInput source="ProductImg" validate={validateTextInput} />
       </SimpleForm>
     </Create>
--- sevla-design-studio/src/components/Admin/AdminProductEdit.js
+++ sevla-design-studio/src/components/Admin/AdminProductEdit.js
@@ -3,7 +3,9 @@ import {
     Edit,
     SimpleForm,
     ReferenceInput,
-    TextInput
+    TextInput,
+    Toolbar,
+    SaveButton
 } from 'react-admin';
 
 //Ensure all input fields are required 
@@ -20,17 +22,24 @@ const validateTextInput = [required()];
 //Ensure price, quantity inputs are required and are numbers
 const validateNum = [required(), number()];
 
+//Create tool bar to edit style for Save Button
+const PostCreateToolbar = props => (
+    <Toolbar {...props}>
+        <SaveButton sx={{width: 200}}/>
+    </Toolbar>
+  );
+  
 const ProductsEdit = () => {
     return (
-        <Edit>
-            <SimpleForm>
+        <Edit sx={{display: 'flex', justifyContent:'center', alignItems:'center', margin: 'auto'}}>
+            <SimpleForm toolbar={<PostCreateToolbar />} sx={{width: 800, alignItems:'center', display: 'flex', padding:'auto'}}>
                 <TextInput source="ProductName" validate={validateTextInput} />
-                <TextInput multiline source="ProductDescription" validate={validateTextInput} />
-                <TextInput multiline source="ProductLongDescription" validate={validateTextInput} />
+                <TextInput multiline source="ProductDescription" validate={validateTextInput} sx={{width: 300}}/>
+                <TextInput multiline source="ProductLongDescription" validate={validateTextInput} sx={{width: 500}}/>
                 <TextInput source="ProductPrice" validate={validateNum} />
-                <TextInput source="ProductSize" />
-                <TextInput source="ProductType" validate={validateTextInput} />
-                <TextInput source="ProductQuantity" validate={validateNum} />
+                <TextInput source="ProductSize" sx={{width: 150}}/>
+                <TextInput source="ProductType" validate={validateTextInput} sx={{width: 150}}/>
+                <TextInput source="ProductQuantity" validate={validateNum} sx={{width: 150}}/>
                 <TextInput source="ProductImg" validate={validateTextInput} />
             </SimpleForm>
         </Edit>
